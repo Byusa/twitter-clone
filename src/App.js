d68fe3fe01fb98a1409ed204be193eefc36b4f6d
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2021-01-25 13:02:22 -0800

    added to the exisitng repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import './App.css';
+import Sidebar from "./Sidebar"
+
+function App() {
+  return (
+    // BEM convention
+    <div className="App">
+      <h1>Hey</h1>
+
+      {/* sidebar*/}
+
+      {/* Feed  */}
+
+      {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Sidebar.css'
+import TwitterIcon from "@"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/*Twitter Icon */}
+
+            {/*SidebarOption*/}
+            {/*SidebarOption*/}
+            {/*SidebarOption*/}
+            {/*SidebarOption*/}
+            {/*SidebarOption*/}
+            {/*SidebarOption*/}
+
+            {/*Button -> Tweet */}
+
+        </div>
+    )
+}
+
+export default Sidebar
