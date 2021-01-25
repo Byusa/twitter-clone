89ca89071c190e66fddadf8523f8fd2d51fc673e
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2021-01-25 13:05:10 -0800

    fixed bug

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Sidebar.css'
-import TwitterIcon from "@"
-
+import TwitterIcon from '@material-ui/icons/Twitter';
 function Sidebar() {
     return (
         <div className="sidebar">
